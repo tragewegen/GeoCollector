2ca58a8a52a141a4c7fb3de03f4b445ed11a2e51
tragewegen <59048676+tragewegen@users.noreply.github.com>
2026-06-24 09:39:12 +0200

    initiële upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'drive-collector-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
